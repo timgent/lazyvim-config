7041d33bf1136cca9a282bc2199347982f8434a1
timgent <tim.gent@gmail.com>
2025-10-19 16:32:02 +0100

    Add keybindings for copying file paths

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -11,3 +11,17 @@ vim.keymap.set("i", "jK", "<Esc>", { desc = "Exit insert mode" })
 vim.keymap.set("i", "KJ", "<Esc>", { desc = "Exit insert mode" })
 vim.keymap.set("i", "Kj", "<Esc>", { desc = "Exit insert mode" })
 vim.keymap.set("i", "kJ", "<Esc>", { desc = "Exit insert mode" })
+
+-- Copy relative file path
+vim.keymap.set("n", "<leader>yp", function()
+  local path = vim.fn.expand("%:.")
+  vim.fn.setreg("+", path)
+  vim.notify("Copied relative path: " .. path)
+end, { desc = "Yank relative file path" })
+
+-- Copy absolute file path
+vim.keymap.set("n", "<leader>yP", function()
+  local path = vim.fn.expand("%:p")
+  vim.fn.setreg("+", path)
+  vim.notify("Copied absolute path: " .. path)
+end, { desc = "Yank absolute file path" })
